474b0e1fa8c50951d63deba17a80459ea549ddc2
Tyler Cook <69126335+cookatyler@users.noreply.github.com>
2020-08-14 19:58:26 -0400

    Add files via upload

---
+++ Player.hpp
@@ -0,0 +1,29 @@
+#include <string>
+
+class Player
+{
+private:
+    int health;
+    int energy;
+    std::string name;
+
+    //In game health / energy items
+    int healthCure = 10;
+    int staminaBoost = 10;
+
+public:
+    int getHealth() const;
+    int getEnergy() const;
+    std::string getName() const;
+    void takeDamage(int damage);
+
+    void setName(std::string newName);
+
+    //Attack method
+    void attack(Player player);
+
+    void collectHealth();
+    void collectEnergy();
+
+    Player(int newHealth, int newEnergy, std::string newName);
+};
